f237c21ca1ba4315f7199de97a9c64b9ad65db9f
Thapsamit <thapliyalamit2001@gmail.com>
2023-04-28 16:58:03 +0530

    simple blocking server

---
+++ README.md
@@ -0,0 +1,4 @@
+## Blocking of Event Loop
+
+- If event loop blocked then it can't serve other requests until previous request is completed
+- Suppose a request takes 10s then other requests will take even more than 10s
--- server.js
+++ server.js
@@ -3,8 +3,25 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+function delay(duration) {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {
+    /*event loop will be blocked and server can't do anything here */
+  } // loop will work till duration is completed
+}
+
 app.get("/", (req, res) => {
   res.send("Performance example");
+});
+
+// LEARNING :
+// 1 - when some request made to the server it will wait for 10 seconds but if seome other request comes at same time then the event loop can't respond to that as it is blocked therefore the further requests will get delayed more and more
+// 2 - And not only this it blockes the other API requests too.
+
+app.get("/timer", (req, res) => {
+  // delay the response here
+  delay(10000); // will be blocked for 10 secons
+  res.send("Your request completed");
 });
 
 app.listen(PORT, () => {
